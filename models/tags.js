18aef66b040a8576ccd77a4dbe8ae7266203d125
Pampattitude <guichan@hotmail.fr>
2014-02-07 20:54:27 +0100

    /models/: updated tags model

--- models/tags.js
+++ models/tags.js
@@ -3,11 +3,9 @@ var mongooseLib = require('mongoose');
 var schema = new mongooseLib.Schema({
     name: {type: String, required: true},
     count: {type: Number, default: 0},
+    hype: {type: Number, default: 0},
 
-    created: {type: Date, default: new Date().toISOString()}
+    created: {type: Date, default: new Date()}
 });
 
-exports = mongooseLib.model('Tag', schema);
-
-var _model = mongooseLib.model('Tag');
-exports._model = _model;
+exports.model = mongooseLib.model('Tag', schema);
