7f7ee865094bd005d00f17d2a2a2e39814bc3741
Pampattitude <guichan@hotmail.fr>
2014-02-07 20:56:45 +0100

    /controllers/ & /views/pages/: updated site menu with hypest tags

--- controllers/site-menu.js
+++ controllers/site-menu.js
@@ -28,7 +28,7 @@ var controller_ = function() {
                 });
             },
             function(serieCallback) {
-                return Tag.find({}).sort({count: -1}).limit(20).exec(function(err, tags) {
+                return Tag.find({}).sort({hype: -1}).limit(20).exec(function(err, tags) {
 	            if (err)
 	                return serieCallback(err);
 
