4088760c09a4a0d9be07d12bb8e35abb7c4c8190
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-02-07 13:51:18 +0100

    /controllers/ & /views/pages/: updated home and site-menu pages content with multiple DB requests

--- controllers/home.js
+++ controllers/home.js
@@ -7,18 +7,45 @@ var utilsLib = require(__dirname + '/../lib/utils');
 var controller_ = function() {
     this.render = function(req, res, renderCallback) {
 	var Article = mongooseLib.model('Article');
+        var articlesModel = require('../models/articles.js');
 
-	return Article.find({}).limit(5).exec(function(err, articles) {
-	    if (err) {
-		consoleLib.error(err);
-		return renderCallback(err);
-	    }
+        return asyncLib.series([
+            function(serieCallback) {
+                return articlesModel.getFeatured(function(err, featured) {
+                    if (err)
+                        return serieCallback(err);
 
-	    res.locals.articleList = articles;
-	    res.locals.inlineStyles.push('home');
-	    res.locals.contentPath = 'pages/home.ejs';
+                    res.locals.featuredArticle = featured;
+                    return serieCallback();
+                })
+            },
+            function(serieCallback) {
+	        return Article.find({}).sort({lastUpdated: -1}).limit(5).exec(function(err, articles) {
+	            if (err)
+                        return serieCallback(err);
+
+                    var featuredCount = 0;
+                    for (var i = 0 ; articles.length > i ; ++i) {
+                        var article = articles[i];
+                        if (true === article.featured)
+                            ++featuredCount;
+                    }
+
+                    if (1 === (articles.length - featuredCount) % 2) {
+                        articles.pop();
+                    }
+
+	            res.locals.articleList = articles;
+	            res.locals.inlineStyles.push('home');
+	            res.locals.contentPath = 'pages/home.ejs';
+
+                    return serieCallback();
+                });
+            },
+	],
+        function(err) {
             return renderCallback();
-	});
+        });
     };
 };
 
--- controllers/site-menu.js
+++ controllers/site-menu.js
@@ -6,13 +6,41 @@ var utilsLib = require(__dirname + '/../lib/utils');
 var controller_ = function() {
     this.render = function(req, res, renderCallback) {
 	var Article = mongooseLib.model('Article');
+	var Tag = mongooseLib.model('Tag');
 
-        return Article.find({}).sort({lastUpdated: 1}).limit(3).exec(function(err, articles) {
-	    if (err)
-	        return renderCallback(err);
+        return asyncLib.series([
+            function(serieCallback) {
+                return Article.find({}).sort({lastUpdated: -1}).limit(3).exec(function(err, articles) {
+	            if (err)
+	                return serieCallback(err);
 
-	    res.locals.latestArticleList = articles;
-	    return renderCallback();
+	            res.locals.latestArticleList = articles;
+	            return serieCallback();
+                });
+            },
+            function(serieCallback) {
+                return Article.find({}).sort({views: -1}).limit(3).exec(function(err, articles) {
+	            if (err)
+	                return serieCallback(err);
+
+	            res.locals.hypestArticleList = articles;
+	            return serieCallback();
+                });
+            },
+            function(serieCallback) {
+                return Tag.find({}).sort({count: -1}).limit(20).exec(function(err, tags) {
+	            if (err)
+	                return serieCallback(err);
+
+	            res.locals.mostUsedTagList = tags;
+	            return serieCallback();
+                });
+            },
+        ],
+        function(err) {
+            if (err)
+                return renderCallback(err);
+            return renderCallback();
         });
     };
 };
