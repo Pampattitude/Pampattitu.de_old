d3c1eb4ad54054b0ea9816fdbcfa2bb0c13f79bf
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-02-10 13:44:50 +0100

    /controllers/: empty password are now handled in the user controller

--- controllers/user.js
+++ controllers/user.js
@@ -8,8 +8,8 @@ var controller_ = function() {
     this.login = function(req, res, loginCallback) {
 	if (!req.body.login)
 	    return loginCallback(new Error('Missing login'));
-	else if (!req.body.password)
-	    return loginCallback(new Error('Missing password'));
+        // Password is not mandatory
+        req.body.password = req.body.password || '';
 
 	return mongooseLib.model('User').findOne({login: req.body.login}, function(err, user) {
 	    if (err)
@@ -17,11 +17,14 @@ var controller_ = function() {
 	    else if (!user)
 		return loginCallback(new Error('User ' + req.body.login + ' unknown'));
 
-	    var crypto = require('crypto');
-	    var shasum = crypto.createHash('sha1');
+            var pass = ''
+            if (req.body.password.length) {
+	        var crypto = require('crypto');
+	        var shasum = crypto.createHash('sha1');
 
-	    shasum.update(req.body.password);
-	    var pass = shasum.digest('hex');
+	        shasum.update(req.body.password);
+	        var pass = shasum.digest('hex');
+            }
 
 	    if (user.password == pass) {
 		req.session.login = req.body.login;
