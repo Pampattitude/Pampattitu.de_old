1e04cada26f1b00ce5d533c6d34653ac36819ccd
Pampattitude <guichan@hotmail.fr>
2014-02-12 23:10:47 +0100

    /views/pages/user/ & /models/: added user description and updated pages style for user

--- models/users.js
+++ models/users.js
@@ -8,6 +8,7 @@ var schema = new mongooseLib.Schema({
     login: {type: String, required: true, index: true, unique: true},
     password: {type: String, default: ''}, // Commentors do not have to be registered
     pictureUrl: {type: String},
+    description: {type: String},
 
     rights: {type: String, default: 'guest', enum: ['guest', 'registered', 'priviledged', 'admin']},
 }, schemaOptions);
