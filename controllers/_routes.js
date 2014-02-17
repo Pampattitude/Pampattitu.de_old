2f8bd5aed19c48d9da45adb9366647ccbf3452f0
Pampattitude <guichan@hotmail.fr>
2014-02-17 21:28:40 +0100

    /controllers/: made routes files more compressed (looks better and simpler)

--- controllers/_routes.js
+++ controllers/_routes.js
@@ -33,73 +33,33 @@ var init_ = function (serverApp) {
     serverApp.post('/login', function(req, res) { return pagesEngine.post({post: userController.login}, req, res); });
     serverApp.post('/logout', function(req, res) { return pagesEngine.post({post: userController.logout}, req, res); });
 
-    serverApp.get('/favicon', function (req, res) {
-	return simpleGet(req, res, 'img/Pmp.ico');
-    });
+    serverApp.get('/favicon', function (req, res) { return simpleGet(req, res, 'img/Pmp.ico'); });
 
-    serverApp.get('/css/bootstrap', function (req, res) {
-	return simpleGet(req, res, 'css/bootstrap.min.css');
-    });
-    serverApp.get('/css/icomoon', function (req, res) {
-	return simpleGet(req, res, 'css/icomoon.css');
-    });
+    serverApp.get('/css/bootstrap', function (req, res) { return simpleGet(req, res, 'css/bootstrap.min.css'); });
+    serverApp.get('/css/icomoon', function (req, res) { return simpleGet(req, res, 'css/icomoon.css'); });
 
-    serverApp.get('/js/jquery', function (req, res) {
-	return simpleGet(req, res, 'js/jquery.js');
-    });
-    serverApp.get('/js/bootstrap', function (req, res) {
-	return simpleGet(req, res, 'js/bootstrap.min.js');
-    });
+    serverApp.get('/js/jquery', function (req, res) { return simpleGet(req, res, 'js/jquery.js'); });
+    serverApp.get('/js/bootstrap', function (req, res) { return simpleGet(req, res, 'js/bootstrap.min.js'); });
 
-    serverApp.get('/img/:file', function (req, res) {
-	return simpleGet(req, res, 'img/' + req.params.file + '.png');
-    });
+    serverApp.get('/img/:file', function (req, res) { return simpleGet(req, res, 'img/' + req.params.file + '.png'); });
 
-    serverApp.get('/fonts/:file', function (req, res) {
-	return simpleGet(req, res, 'fonts/' + req.params.file);
-    });
+    serverApp.get('/fonts/:file', function (req, res) { return simpleGet(req, res, 'fonts/' + req.params.file); });
 
-    serverApp.get('/humans', function (req, res) {
-	return res.redirect('/humans.txt');
-    });
-    serverApp.get('/humans.txt', function (req, res) {
-	return simpleGet(req, res, 'humans.txt');
-    });
-    serverApp.get('/robots', function (req, res) {
-	return res.redirect('/robots.txt');
-    });
-    serverApp.get('/robots.txt', function (req, res) {
-	return simpleGet(req, res, 'robots.txt');
-    });
+    serverApp.get('/humans', function (req, res) { return res.redirect('/humans.txt'); });
+    serverApp.get('/humans.txt', function (req, res) { return simpleGet(req, res, 'humans.txt'); });
+    serverApp.get('/robots', function (req, res) { return res.redirect('/robots.txt'); });
+    serverApp.get('/robots.txt', function (req, res) { return simpleGet(req, res, 'robots.txt'); });
 
 
-    serverApp.get('/js/*', function (req, res) {
-	res.writeHead(404, {});
-	return res.end();
-    });
-    serverApp.get('/css/*', function (req, res) {
-	res.writeHead(404, {});
-	return res.end();
-    });
-    serverApp.get('/img/*', function (req, res) {
-	res.writeHead(404, {});
-	return res.end();
-    });
-    serverApp.get('/fonts/*', function (req, res) {
-	res.writeHead(404, {});
-	return res.end();
-    });
+    serverApp.get('/js/*', function (req, res) { res.writeHead(404, {}); return res.end(); });
+    serverApp.get('/css/*', function (req, res) { res.writeHead(404, {}); return res.end(); });
+    serverApp.get('/img/*', function (req, res) { res.writeHead(404, {}); return res.end(); });
+    serverApp.get('/fonts/*', function (req, res) { res.writeHead(404, {}); return res.end(); });
 
-    serverApp.get('/404', function (req, res) {
-	return pagesEngine.render({content: errorController.render404}, req, res);
-    });
+    serverApp.get('/404', function (req, res) { return pagesEngine.render({content: errorController.render404}, req, res); });
 
-    serverApp.get('*', function (req, res) {
-	return pagesEngine.render({content: errorController.render404}, req, res);
-    });
-    serverApp.post('*', function (req, res) {
-	return pagesEngine.render({content: errorController.render404}, req, res);
-    });
+    serverApp.get('*', function (req, res) { return pagesEngine.render({content: errorController.render404}, req, res); });
+    serverApp.post('*', function (req, res) { return pagesEngine.render({content: errorController.render404}, req, res); });
 };
 
 exports.init = init_;
