ce495472d566618513edf39aaed3ac58d79232db
Pampattitude <guichan@hotmail.fr>
2014-02-18 00:17:41 +0100

    /controllers/ & /views/pages/article/: added view and renderer for creating/editing articles

--- controllers/article.js
+++ controllers/article.js
@@ -70,6 +70,32 @@ var controller_ = function() {
 	});
     };
 
+    this.renderEdit = function(req, res, renderCallback) {
+	var Article = mongooseLib.model('Article');
+	var articlesModel = require('../models/articles.js');
+
+	var technicalName = req.params.technicalName;
+
+	if (!technicalName) {
+	    // res.locals.inlineStyles.push('article');
+	    res.locals.contentPath = 'pages/article/edit.ejs';
+
+            return renderCallback();
+	}
+
+	return Article.getByTechnicalName(technicalName, function(err, article) {
+	    if (err)
+		return renderCallback(err);
+	    else if (!article)
+		return renderCallback('Could not find article ' + technicalName);
+
+	    res.locals.article = article;
+	    // res.locals.inlineStyles.push('article');
+	    res.locals.contentPath = 'pages/article/edit.ejs';
+
+            return renderCallback();
+	});
+    };
 };
 
 exports.Controller = controller_;
