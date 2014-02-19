f2871b0d04e81a3f42becccbcf844808e5685fd6
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-02-19 12:41:07 +0100

    /: added support for Ctrl+C and graceful close / cleanup

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongooseLib.connection.on('error', function (err) {
 	// Handle error
 	consoleLib.error('Could not open DB connection: ' + err);
 
-	mongooseLib.close();
+	mongooseLib.connection.close();
 	mongooseLib.connect(databaseUri);
 });
 mongooseLib.connection.once('open', function () {
@@ -36,19 +36,19 @@ mongooseLib.connection.once('open', function () {
 	consoleLib.log('Collections sync\'ed');
 });
 
-var serverApp = expressLib();
+var app = expressLib();
 
-serverApp.configure(function() {
-    serverApp.use(expressLib.errorHandler({ dumpExceptions: true, showStack: true }));
-    serverApp.use(expressLib.logger('dev'));
+app.configure(function() {
+    app.use(expressLib.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(expressLib.logger('dev'));
 
-    serverApp.use(expressLib.bodyParser());
-    serverApp.use(expressLib.methodOverride());
+    app.use(expressLib.bodyParser());
+    app.use(expressLib.methodOverride());
 
-    serverApp.use(expressLib.cookieParser());
-    serverApp.use(expressLib.session({secret: '7iGofFxdVeCafeq35BDrOdoV'}));
+    app.use(expressLib.cookieParser());
+    app.use(expressLib.session({secret: '7iGofFxdVeCafeq35BDrOdoV'}));
 
-    serverApp.use(function(req, res, next) {
+    app.use(function(req, res, next) {
 	if (!req.params)  req.params = {};
 	if (!req.body)    req.body = {};
 	if (!req.session) req.session = {};
@@ -56,9 +56,31 @@ serverApp.configure(function() {
 	return next();
     });
 
-    require('./controllers/_routes.js').init(serverApp);
-    serverApp.use(serverApp.router);
-    serverApp.enable('jsonp callback');
+    require('./controllers/_routes.js').init(app);
+    app.use(app.router);
+    app.enable('jsonp callback');
 });
 
-serverApp.listen(7337);
+var server = app.listen(7337);
+
+function cleanUp() {
+    return server.close();
+}
+server.on('close', function() {
+    winstonLib.info('Closing database connection...');
+    mongooseLib.connection.close(function() {
+        winstonLib.info('Database connection closed, exiting...');
+        return process.exit(0);
+    });
+
+    // Handle ungracefull close
+    var maxCloseTime = 5*1000 // 5s;
+    return setTimeout( function() {
+        winstonLib.error('Could not close connections in ' + parseInt(maxCloseTime / 1000) + 's, forcing shut down...');
+        return process.exit(1);
+    }, maxCloseTime);
+});
+
+process.on('SIGINT',  cleanUp)
+process.on('SIGTERM', cleanUp) 
+
