dc0228705e38ba69c5a9fd65e2ad674fbf001f79
Pampattitude <guichan@hotmail.fr>
2014-02-23 15:42:39 +0100

    /: added cluster support

--- app.js
+++ app.js
@@ -7,69 +7,88 @@ var fsLib = require('fs');
 var ejsLib = require('ejs');
 var jsonLib = require('JSON');
 
+var clusterLib = require('cluster');
+
 var winstonLib = require('winston');
 winstonLib.remove(winstonLib.transports.Console);
 winstonLib.add(winstonLib.transports.Console, {level: 'silly', prettyPrint: true, colorize: true, timestamp: true});
 
 var mongooseLib = require('mongoose');
+var databaseUri = 'mongodb://localhost/bmb-home';
 
 var consoleLib = require('./lib/console');
 
-var databaseUri = 'mongodb://localhost/bmb-home';
+if (clusterLib.isMaster) {
+    var cluserPerCpu = 1;
+    var clusterCount = parseInt(require('os').cpus().length * cluserPerCpu) || 1;
 
-mongooseLib.connect(databaseUri);
-mongooseLib.connection.on('error', function (err) {
-    // Handle error
-    consoleLib.error('Could not open DB connection: ' + err);
-
-    mongooseLib.connection.close();
-    mongooseLib.connect(databaseUri);
-});
-mongooseLib.connection.once('open', function () {
-    // Handle open;
-    consoleLib.log('DB connection open');
-
-    require('./models/articles.js').model;
-    require('./models/comments.js').model;
-    require('./models/tags.js').model;
-    require('./models/users.js').model;
-
-    consoleLib.log('Collections sync\'ed');
-});
-
-var app = expressLib();
-app.configure(function() {
-    app.disable('x-powered-by');
-
-    app.use(expressLib.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.use(expressLib.logger('dev'));
-
-    app.use(expressLib.bodyParser());
-    app.use(expressLib.methodOverride());
-
-    var mongoStore = require('connect-mongo')(expressLib);
-    app.use(expressLib.cookieParser());
-    app.use(expressLib.session({
-        store: new mongoStore ({
-            url: databaseUri + '/sessions'
-        },
-	function() {
-	    consoleLib.info("MongoStore connected!");
-	}),
-        secret: '7iGofFxdVeCafeq35BDrOdoV',
-    }));
-
-    app.use(function(req, res, next) {
-	if (!req.params)  req.params = {};
-	if (!req.body)    req.body = {};
-	if (!req.session) req.session = {};
-	if (!res.locals)  res.locals = {};
-	return next();
+    clusterLib.on('fork', function(worker) {
+	consoleLib.info('Worker ' + worker.id + ' created');
+    });
+    clusterLib.on('exit', function (worker) {
+	consoleLib.warn('Worker ' + worker.id + ' died, forking a new one');
+	clusterLib.fork();
     });
 
-    require('./controllers/_routes.js').init(app);
-    app.use(app.router);
-    app.enable('jsonp callback');
-});
+    for (var i = 0 ; i < clusterCount ; ++i) {
+        clusterLib.fork();
+    }
+}
+else {
+    mongooseLib.connect(databaseUri);
+    mongooseLib.connection.on('error', function (err) {
+	// Handle error
+	consoleLib.error('Could not open DB connection: ' + err);
 
-var server = app.listen(7337);
+	mongooseLib.connection.close();
+	mongooseLib.connect(databaseUri);
+    });
+    mongooseLib.connection.once('open', function () {
+	// Handle open;
+	consoleLib.log('DB connection open');
+
+	require('./models/articles.js').model;
+	require('./models/comments.js').model;
+	require('./models/tags.js').model;
+	require('./models/users.js').model;
+
+	consoleLib.log('Collections sync\'ed');
+    });
+
+    var app = expressLib();
+    app.configure(function() {
+	app.disable('x-powered-by');
+
+	app.use(expressLib.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(expressLib.logger('dev'));
+
+	app.use(expressLib.bodyParser());
+	app.use(expressLib.methodOverride());
+
+	var mongoStore = require('connect-mongo')(expressLib);
+	app.use(expressLib.cookieParser());
+	app.use(expressLib.session({
+            store: new mongoStore ({
+		url: databaseUri + '/sessions'
+            },
+	    function() {
+		consoleLib.info("MongoStore connected!");
+	    }),
+            secret: '7iGofFxdVeCafeq35BDrOdoV',
+	}));
+
+	app.use(function(req, res, next) {
+	    if (!req.params)  req.params = {};
+	    if (!req.body)    req.body = {};
+	    if (!req.session) req.session = {};
+	    if (!res.locals)  res.locals = {};
+	    return next();
+	});
+
+	require('./controllers/_routes.js').init(app);
+	app.use(app.router);
+	app.enable('jsonp callback');
+    });
+
+    var server = app.listen(7337);
+}
