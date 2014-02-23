c02276b9f33e7b871d07ccfb0feb736786ce4563
Pampattitude <guichan@hotmail.fr>
2014-02-23 20:29:36 +0100

    /controllers/: experimental search algorithm, might be slow (explicit N*N + regex)

--- controllers/search.js
+++ controllers/search.js
@@ -10,7 +10,7 @@ var controller_ = function() {
 	var articlesPerPage = 3;
 	var pageNumber = ((req.params.page - 1) >= 0) ? (req.params.page - 1) : 0;
 	var offset = (pageNumber || 0) * articlesPerPage;
-
+/*
 	var data = (req.params.data ? req.params.data.split(' ') : []);
 	var page = req.params.page;
 	return mongooseLib.model('Article').find({tags: {$in: data}}).sort({views: -1}).exec(function(err, articleList) {
@@ -25,6 +25,61 @@ var controller_ = function() {
 	    res.locals.contentPath = 'pages/search/content.ejs';
 
 	    return renderCallback();
+	});
+*/
+	var pointsForTitle =   8;
+	var pointsForCaption = 3;
+	var pointsForContent = 1;
+	var pointsForTag =     5;
+
+	var data = (req.params.data ? req.params.data.split(' ') : []);
+	var page = req.params.page;
+	return mongooseLib.model('Article').find({}).exec(function(err, articleList) {
+	    if (err)
+		return renderCallback(err);
+
+	    return asyncLib.eachSeries(articleList, function(article, articleCallback) {
+		article.points = 0;
+
+		for (var j = 0 ; data.length > j ; ++j) {
+		    for (var i = 0 ; article.tags.length > i ; ++i) {
+			if (data[j] == article.tags[i]) {
+			    article.points += pointsForTag;
+			}
+		    }
+
+		    console.log('points before ' + article.points);
+		    var tagSearch = new RegExp('(' + data[j] + ')', 'i');
+		    var titleMatch = article.title.match(tagSearch);
+		    var captionMatch = article.caption.match(tagSearch);
+		    var contentMatch = article.content.match(tagSearch);
+		    article.points += (titleMatch ? titleMatch.length - 1 : 0) * pointsForTitle;
+		    console.log('points after title ' + article.points);
+		    article.points += (captionMatch ? captionMatch.length - 1 : 0) * pointsForCaption;
+		    console.log('points after caption ' + article.points);
+		    article.points += (contentMatch ? contentMatch.length - 1 : 0) * pointsForContent;
+		    console.log('points after content ' + article.points);
+		}
+
+		consoleLib.debug('Article "' + article.title + '" has ' + article.points + ' points');
+
+		return articleCallback();
+	    },
+	    function(err) {
+		if (err)
+		    return renderCallback(err);
+
+		articleList.sort(function(a, b) { return b.points - a.points; });
+
+		res.locals.articleList = articleList.slice(offset, offset + articlesPerPage);
+		res.locals.pageCount = articleList.length / articlesPerPage;
+		res.locals.actualPage = pageNumber;
+		res.locals.searchString = req.params.data;
+		// res.locals.inlineStyles.push('search');
+		res.locals.contentPath = 'pages/search/content.ejs';
+
+		return renderCallback();
+	    });
 	});
     };
 };
