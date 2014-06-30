097b8183e1924a49926889bf5d534ab7a3abeae0
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-06-30 14:17:50 +0200

    /: app now listens to port 7337, both in debug and in production (nginx setup in production)

--- app.js
+++ app.js
@@ -117,9 +117,5 @@ else {
         app.enable('jsonp callback');
     });
 
-    var server = null;
-    if ('debug' == process.env.NODE_ENV)
-        server = app.listen(7337);
-    else
-        server = app.listen(80);
+    var server = app.listen(7337);
 }
