9dd18e7372e3ecc199bd6239096990aaea45f65d
Guillaume Delahodde <guichan@hotmail.fr>
2014-07-12 13:08:43 +0200

    /controllers/front/ & /views/pages/front/: made button menu configurable for front

--- controllers/front/common.js
+++ controllers/front/common.js
@@ -4,5 +4,7 @@ var setCommonFields = function(res) {
     res.locals.app = 'front';
 
     res.locals.inlineStyles.push('/css/pmp.front');
+
+    res.locals.buttonMenu = 'pages/front/button-menu';
 };
 exports.setCommonFields = setCommonFields;
