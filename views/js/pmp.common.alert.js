4a5f738c0ffbaf2c5d13cad4e5c529d8a2dccc8d
Guillaume Delahodde <guichan@hotmail.fr>
2014-07-13 11:52:18 +0200

    /views/js/: made alert JS script a little bit less constraining

--- views/js/pmp.common.alert.js
+++ views/js/pmp.common.alert.js
@@ -3,7 +3,7 @@
 
     return $(document).ready(function() {
         $('.alert-menu .alert .close').click(function() {
-            return $(this).parent('div.alert').slideToggle(200, function() {
+            return $(this).parent('.alert').slideToggle(200, function() {
                 return $(this).remove();
             });
         });
