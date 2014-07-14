2519d058214807a93efbf5a7a5f8f46aef6240d8
Guillaume Delahodde <guichan@hotmail.fr>
2014-07-14 12:40:51 +0200

    /controllers/back/: removed a temporary log from reports page

--- controllers/back/reports.js
+++ controllers/back/reports.js
@@ -41,8 +41,6 @@ var controller_ = function() {
             return postCallback(new Error('Missing report ID from form'));
         }
 
-        console.log(req.body);
-
         var newState = null;
         if ('undefined' !== typeof req.body.stateOpen) newState = 'open';
         if ('undefined' !== typeof req.body.stateInProgress) newState = 'inProgress';
