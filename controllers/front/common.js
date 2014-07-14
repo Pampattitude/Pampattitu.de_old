9b88aeb6fef7ca477ab5618d2194f2d65bf63ad4
Guillaume Delahodde <guichan@hotmail.fr>
2014-07-14 12:44:16 +0200

    /controllers/front/: damn! Added Twitter script in front pages

--- controllers/front/common.js
+++ controllers/front/common.js
@@ -4,6 +4,7 @@ var setCommonFields = function(res) {
     res.locals.app = 'front';
 
     res.locals.inlineStyles.push('/css/pmp.front');
+    res.locals.inlineScripts.push('/js/pmp.twitter');
 
     res.locals.buttonMenu = 'pages/front/button-menu';
 };
