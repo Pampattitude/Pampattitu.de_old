0ba27d764ee3ef9d6e6907304d4dada02844032c
Guillaume Delahodde <guichan@hotmail.fr>
2014-07-26 20:01:19 +0200

    /: added dirname variable in launch.sh for service

--- launch.sh
+++ launch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-NODE_ENV=debug forever start --minUptime 3000 --spinSleepTime 30000 -l /data/log/beta-website.log -a front.js
-NODE_ENV=debug forever start --minUptime 3000 --spinSleepTime 30000 -l /data/log/beta-backoffice.log -a back.js
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+NODE_ENV=debug forever start --minUptime 3000 --spinSleepTime 30000 -l /data/log/beta-website.log -a "$SCRIPTPATH/front.js"
+NODE_ENV=debug forever start --minUptime 3000 --spinSleepTime 30000 -l /data/log/beta-backoffice.log -a "$SCRIPTPATH/back.js"
