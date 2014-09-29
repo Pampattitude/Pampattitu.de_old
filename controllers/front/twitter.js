9a8f355c461fcb6d4ebdd4bc31671b299b77084c
Guillaume Delahodde <guichan@hotmail.fr>
2014-09-29 13:25:07 +0200

    /controllers/front/: udpated Twitter links fetch

--- controllers/front/twitter.js
+++ controllers/front/twitter.js
@@ -20,17 +20,21 @@ var controller_ = function() {
             access_token_secret: '6vme2Q77BZrIwUeXrWBzg9v9MRyPuq3kuMhPEOmpBbzbo',
         });
         return twitter.get('/statuses/user_timeline.json', {screen_name: 'Pampattitude', count: 5}, function(err, tweets) {
-            var latestTweet = null;
-            var latestTweetLink = 'http://twitter.com/Pampattitude';
+            var latestTweets = [];
 
             if (err || !tweets.length)
                 sessionLib.pushMessage(res, 'warning', 'Could not get latest tweet.');
             else {
-                latestTweet = tweets[0].text; //.replace(/(http[s]?:\/\/[^ ]+)/g, '<u>$1</u>').replace(/#([0-9A-Za-z\-_]+)/g, '<i>#$1</i>');
-                latestTweetLink = 'https://twitter.com/' + tweets[0].user.screen_name + '/status/' + tweets[0].id_str;
+                for (var i = 0 ; tweets.length > i ; ++i) {
+                    var tweet = tweets[i];
+                    latestTweets.push({
+                        text: tweet.text, //.replace(/(http[s]?:\/\/[^ ]+)/g, '<u>$1</u>').replace(/#([0-9A-Za-z\-_]+)/g, '<i>#$1</i>'),
+                        link: 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str,
+                    });
+                }
             }
 
-            return ajaxCallback(null, {latestTweet: latestTweet, latestTweetLink: latestTweetLink});
+            return ajaxCallback(null, {latestTweets: latestTweets});
         });
     };
 };
