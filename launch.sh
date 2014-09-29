c41aaa4ce0d7abb0bb5b2e78436cb7efd16b6ac8
Guillaume Delahodde <guichan@hotmail.fr>
2014-09-29 13:36:09 +0200

    /: updated launch.sh script with more options, like a service

--- launch.sh
+++ launch.sh
@@ -1,7 +1,56 @@
-#!/bin/bash
+#!/bin/sh
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+start() {
+    echo "Starting..."
 
-NODE_ENV=debug forever start --minUptime 3000 --spinSleepTime 30000 -l /data/log/beta-website.log -a "$SCRIPTPATH/front.js"
-NODE_ENV=debug forever start --minUptime 3000 --spinSleepTime 30000 -l /data/log/beta-backoffice.log -a "$SCRIPTPATH/back.js"
+    FOREVERNBR=`forever list | wc -l`
+
+    if [ "$FOREVERNBR" != "1" ]; then
+        echo "Could not start, scripts already running under forever:" 1>&2
+        forever list
+        return 1
+    fi
+
+    SCRIPT=$(readlink -f "$0")
+    SCRIPTPATH=$(dirname "$SCRIPT")
+
+    NODE_ENV=debug forever start --minUptime 3000 --spinSleepTime 30000 -l /data/log/beta-website.log -a "$SCRIPTPATH/front.js"
+    NODE_ENV=debug forever start --minUptime 3000 --spinSleepTime 30000 -l /data/log/beta-backoffice.log -a "$SCRIPTPATH/back.js"
+
+    echo "Started!"
+
+    return 0
+}
+
+stop() {
+    echo "Stopping..."
+    forever stopall
+    echo "Stopped!"
+    return 0
+}
+
+restart() {
+    stop && echo && start
+}
+
+list() {
+    forever list
+    return 0
+}
+
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        restart
+        ;;
+    list)
+        list
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart|list}"
+esac
